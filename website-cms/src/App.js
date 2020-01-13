a465bc4cc0bec432497c6985b1e2f419110b3090
Wyatt Focht <wyatt.focht@duke.edu>
2020-01-13 14:42:00 -0500

    got tabs working

--- website-cms/src/App.js
+++ website-cms/src/App.js
@@ -1,19 +1,21 @@
-import {React, useState} from 'react';
+import React from 'react';
 import AddMembers from './AddMembers';
 import AddPapers from './AddPapers';
 import AddProjects from './AddProjects';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {useState} from 'react';
 
 
 
 function App() {
   const [key, setKey] = useState('members');
-
+ 
   return (
     <Tabs id="controlled-tabs" activeKey={key} onSelect={k => setKey(k)}>
       <Tab eventKey="members" title="Members">
-        <AddMembers/>
+      <AddMembers/>
       </Tab>
       <Tab eventKey="papers" title="Papers">
         <AddPapers />
