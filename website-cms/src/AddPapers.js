dec4f97331847ddbf985c31e4865a7769b339711
Wyatt Focht <wyatt.focht@duke.edu>
2020-01-16 15:20:00 -0500

    added paper form and changed formatting

--- website-cms/src/AddPapers.js
+++ website-cms/src/AddPapers.js
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const AddPapers = (props) => {
   return (
     <div>
     <h1>Add New Paper to the Paper Page</h1>
+    <Form>
+            {/*Text input for Authors*/}
+      <FormGroup>
+        <Label for="Authors">Author(s) (First Last, First Last, ...)</Label>
+        <Input type="text" name="Authors" id="Authors" placeholder="ex. Yasa Baig, Luke Truitt" />
+      </FormGroup>
+            {/*Textarea input for Abstracts*/}
+      <FormGroup>
+        <Label for="Abstracts">Abstracts </Label>
+        <Input type="textarea" name="Abstracts" id="Abstracts" placeholder="You don't need an example." />
+      </FormGroup>
+            {/*Text input for DOI*/} 
+      <FormGroup>
+        <Label for="DOI">Digitial Object Identifier (DOI) </Label>
+        <Input type="text" name="DOI" id="DOI" placeholder="ex. 10.1080/01972240490951890" />
+      </FormGroup>
+            {/*Textarea input for Publication Date*/}
+      <FormGroup>
+        <Label for="PublicationDate">Publication Date </Label>
+        <Input type="text" name="PublicationDate" id="PublicationDate" placeholder="ex. Fill in later." />
+      </FormGroup>
+            {/*Textarea input for Link To Full Text*/}
+            <FormGroup>
+        <Label for="FullLink">Link To Full Text </Label>
+        <Input type="textarea" name="FullLink" id="FullLink" placeholder="ex. http://www.machinelearning.ru/wiki/images/0/07/Langley00crafting.pdf" />
+      </FormGroup>
+      </Form>
+     
+            {/*Form Submit Button*/}
+      <Button>Add Paper</Button>
     </div>
   );
 }
--- website-cms/src/App.js
+++ website-cms/src/App.js
@@ -6,6 +6,7 @@ import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from 'react';
+import "./Forms.css";
 
 
 
