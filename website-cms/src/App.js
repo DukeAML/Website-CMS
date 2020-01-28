efab58fc9887584fede13d4d09ddd9fc63772f8e
Wyatt Focht <wyatt.focht@duke.edu>
2020-01-27 19:26:24 -0500

    push push

--- website-cms/src/App.js
+++ website-cms/src/App.js
@@ -8,44 +8,39 @@ import Header from './Header';
 import Footer from './Footer';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
+import Container from "react-bootstrap/Container"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {useState} from 'react';
 import "./Forms.css";
 
-<<<<<<< HEAD
-=======
-
-
-
->>>>>>> f3069f9043285b164dfc4ab2e50353542a6beb47
 function App() {
   const [key, setKey] = useState('home');
  
   return (
-    <div>
-    <header>
-    <Header/> 
-    </header>
-    <main className="body">
-    <Tabs className = "tab" id="controlled-tabs" activeKey={key} onSelect={k => setKey(k)}>
-      <Tab eventKey="home" title="Home">
-      <LandingPage/> 
-      </Tab>
-      <Tab eventKey="members" title="Members">
-      <AddMembers/><SearchExisting/>
-      </Tab>
-      <Tab eventKey="projects" title="Projects">
-        <AddProjects />
-      </Tab>
-      <Tab eventKey="papers" title="Papers">
-        <AddPapers />
-      </Tab>
-    </Tabs>
-    </main>
-    <footer>
-      <Footer/>
-    </footer>
-    </div>
+    <Container>
+      <header>
+      <Header /> 
+      </header>
+      <main className="body">
+      <Tabs  className = "tab" id="controlled-tabs" activeKey={key} onSelect={k => setKey(k)}>
+        <Tab eventKey="home" title="Home">
+        <LandingPage/> 
+        </Tab>
+        <Tab eventKey="members" title="Members">
+        <AddMembers/><SearchExisting/>
+        </Tab>
+        <Tab eventKey="projects" title="Projects">
+          <AddProjects />
+        </Tab>
+        <Tab eventKey="papers" title="Papers">
+          <AddPapers />
+        </Tab>
+      </Tabs>
+      </main>
+      <footer>
+        <Footer/>
+      </footer>
+    </Container>
   );
 }
 
