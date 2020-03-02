5c97a7c925f47a0c8d2f7af786084c792000693a
Wyatt Focht <wyatt.focht@duke.edu>
2020-03-02 16:55:13 -0500

    got members format to work

--- src/Members/SearchExistingMembers.js
+++ src/Members/SearchExistingMembers.js
@@ -1,42 +1,90 @@
 import React from 'react';
 import {useState} from 'react';
-
+import axios from "axios";
 import Table from 'react-bootstrap/Table';
 import "../Forms.css";
 import PaginateMembers from "./PaginateMembers.js";
 
-const SearchExistingMembers = (props) => {
+export default class SearchExistingMembers extends React.Component {
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
+      state = {
+        members: []
+    };
 
-  const toggle = () => setDropdownOpen(prevState => !prevState);
 
-  return (
-    <div >
-    <h1>Search Existing Members</h1>
-    
-    <div className = "table-wrapper-scroll-y my-custom-scrollbar" >  
-    
-    <Table striped bordered hover responsive >
-    <thead>
-    <tr >
+    componentDidMount() {
+        this.getMembers();
+    }
+
+    getMembers =  async () => {
+        var members = []
+        let response = await axios.get('https://dukeappml.herokuapp.com//users')
+        this.setState({members: response.data}) 
+        console.log("successsssss");     
+        console.log(response);       
+
+    };
+
+    getJSX = () => {
+        var array = [];
+        array.push(
+          <thead>
+          <tr>
       <th>#</th>
       <th>First Name</th>
       <th>Last Name</th>
-    </tr>
-  </thead>
+      </tr>
+      </thead>
+        );
+        if (this.state.members.length > 0){
+        for(let i = 0;i<this.state.members.length;i++){
+            array.push(
+                
+                <tr onClick = {console.log("yeetyah")}>
+                
+                <td>{i+1}</td>
+                <td> {this.state.members[i].firstName} </td>
+                <td>{this.state.members[i].lastName}</td>
+                </tr>
+
+
+            );
+        }
+    }
+        
+        return(
+            <Table>{array}</Table>
+        );
+      
+    };
+
+
+
+  render() {
+    return(
+    <div>
+    <h1>Search Existing Members</h1>
+    
+    <div className = "table-wrapper-scroll-y my-custom-scrollbar" >  
+    {/*beginning of table*/}
+    <Table striped bordered hover responsive >
+      {/*header of table*/}
+    
+      {/*end of header of table*/}
+
+      {/*body of table with content from axios*/}
       <tbody className = "tableb">
-        <PaginateMembers/>    
+      <div>
+        {this.getJSX()}
+        </div>   
       </tbody>
+      {/*end body of table with content from axios*/}
     </Table>
   
       
-</div>
+  </div>
     </div>
   );
 }
 
-export default SearchExistingMembers;
-
-
-
+}
